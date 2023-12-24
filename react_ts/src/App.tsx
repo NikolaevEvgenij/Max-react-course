d6726068edd751847105965775158daa2a9bdd88
XpysTEEAM <nikolaev-evgenij@bk.ru>
2023-12-24 21:20:40 +0300

    Tanstach Query practice started

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,12 +1,10 @@
-import { useContext } from "react";
 
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import { TodosContext } from "./store/todo-context";
 
 function App() {
-   const context = useContext(TodosContext);
+   
    return (
       <>
          <NewTodo />
